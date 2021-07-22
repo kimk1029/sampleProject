1af75afbcb6cf6d20c4bc8bfc99816cfa334b6c1
kimk1029 <kimk1029@neowiz.com>
2021-07-22 23:38:50 +0900

    react navigation init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import React, { FC } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\n Cmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu`
-});
-const App: FC = () => {
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeStackNavigatoer from './navigators/HomeStackNavigators';
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <NavigationContainer>
+      <HomeStackNavigatoer />
+    </NavigationContainer>
   );
-};
-export default App;
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
+}
